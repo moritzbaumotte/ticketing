5a7d3e8885b3afbbe63eb84285a68ddd18904aaa
baumotte <moritz@baumotte.com>
2018-10-08 16:55:58 +0200

    setting up for db connection

--- src/com/baumotte/ticketing/endpoints/TicketEP.java
+++ src/com/baumotte/ticketing/endpoints/TicketEP.java
@@ -9,7 +9,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 
 import com.baumotte.ticketing.entities.Ticket;
