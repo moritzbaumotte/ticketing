b7478d5b44dee2ffd9c6272a9390cc8842c1e869
baumotte <moritz@baumotte.com>
2018-10-09 15:58:55 +0200

    connecting to db service works

--- src/com/baumotte/ticketing/endpoints/TicketEP.java
+++ src/com/baumotte/ticketing/endpoints/TicketEP.java
@@ -1,6 +1,7 @@
 package com.baumotte.ticketing.endpoints;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -9,7 +10,11 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.baumotte.ticketing.entities.Ticket;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -20,18 +25,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Path("/tickets")
 public class TicketEP {
 	private final ObjectMapper mapper;
+	private final Client client;
 	
 	public TicketEP() {
 		this.mapper = new ObjectMapper();
+		this.client = ClientBuilder.newClient();
 	}
 	
 	@GET
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getTickets() throws JsonProcessingException {
-		Ticket dummyTicket = new Ticket(0, "moritz@baumotte.com", "test title", "test description");
+	public Response getTickets() throws JsonProcessingException {
+		//Ticket dummyTicket = new Ticket(0, "moritz@baumotte.com", "test title", "test description");
 		//get tickets
-		return mapper.writeValueAsString(dummyTicket);
+		WebTarget target = client.target("http://localhost:8080/dbconnector/rest/queries");
+		ArrayList<Ticket> tickets = target.request().accept("application/json").get().readEntity(ArrayList.class);
+		
+		return Response.status(Response.Status.OK).entity(tickets).build();
 	}
 	
 	@POST
