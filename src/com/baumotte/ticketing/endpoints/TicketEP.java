6c804b0d7ddfd1b2344ecb8b5124a60907614af9
baumotte <moritz@baumotte.com>
2018-10-17 15:51:56 +0200

    done

--- src/com/baumotte/ticketing/endpoints/TicketEP.java
+++ src/com/baumotte/ticketing/endpoints/TicketEP.java
@@ -1,16 +1,13 @@
 package com.baumotte.ticketing.endpoints;
 
-import java.io.IOException;
 import java.util.ArrayList;
-
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -35,14 +32,13 @@ public class TicketEP {
 	}
 	
 	@GET
-	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/{user}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getTickets(@QueryParam("user") String email) throws JsonProcessingException {
+	public Response getTickets(@PathParam("user") String email) throws JsonProcessingException {
 		//get tickets
 		if(email != null) {
-			WebTarget target = client.target(getURL("dbconnector_ticketing") + "/tickets");
+			WebTarget target = client.target(getURL("dbconnector_ticketing") + "/" + email + "/tickets");
 			ArrayList<Ticket> tickets = target
-					.queryParam("user", email)
 					.request()
 					.accept(MediaType.APPLICATION_JSON)
 					.get()
@@ -54,6 +50,25 @@ public class TicketEP {
 		}
 	}
 	
+	@GET
+	@Path("/{user}/ticket/{id}/responses")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getResponses(@PathParam("user") String email, @PathParam("id") int id) {
+		if(email != null) {
+			WebTarget target = client.target(getURL("dbconnector_ticketing") + "/" + email + "/tickets/" + id + "/responses");
+			ArrayList<com.baumotte.ticketing.entities.Response> responses = target
+					.request()
+					.accept(MediaType.APPLICATION_JSON)
+					.get()
+					.readEntity(ArrayList.class);
+			
+			return Response.status(Response.Status.OK).entity(responses).build();
+		}else {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+	}
+	
+	/*
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -80,6 +95,7 @@ public class TicketEP {
 		//mark ticket as complete
 		return "{'status': 'success'}";
 	}
+	*/
 	
 	private String getURL(String serviceName) {
 		return client.target("http://localhost:8080/servicebroker/rest/servicebroker/" + serviceName)
